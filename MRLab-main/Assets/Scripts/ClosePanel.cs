bfd363631f867dd48db4bc53077f20a26c547f2c
Sebastian Martinez <sebasmartinezr2014@gmail.com>
2025-11-14 14:48:07 -0500

    creo que no se movio nada

--- MRLab-main/Assets/Scripts/ClosePanel.cs
+++ MRLab-main/Assets/Scripts/ClosePanel.cs
@@ -1,27 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ClosePanel : MonoBehaviour
 {
     public GameObject panel;
-    public GameObject apiReciever;
     public Text buttonText;
 
     public void SetActive()
     {
-        if (panel.activeSelf){
+        if (panel.activeSelf)
+        {
             panel.SetActive(false);
-            apiReciever.SetActive(false);
             buttonText.text = "Open";
         }
-        else { 
+        else
+        {
             panel.SetActive(true);
-            apiReciever.SetActive(true);
             buttonText.text = "Close";
         }
     }
-
-
 }
